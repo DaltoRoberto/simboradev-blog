d9ea67e2e0b74beae063a66f6cc93fc177bb4544
Sergio Danilo Jr <sergiodanilojr@hotmail.com>
2023-09-18 14:57:50 -0300

    chore: publish docker environment files

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Route::post('login', \App\Http\Controllers\Auth\AccessController::class)->name('
 Route::get('publicacoes', [\App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
 Route::get('publicacoes/{slug}', [\App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
 Route::get('search', [\App\Http\Controllers\PostController::class, 'search'])->name('posts.search');
+
 Route::middleware(['auth'])->prefix('admin')->group(function (){
     Route::get('/', \App\Http\Controllers\Admin\DashboardController::class);
 });
